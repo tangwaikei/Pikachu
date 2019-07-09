30d5771f7f1c62b657609a100297ec0a793dfe87
deng_wei_qi <dengweiqi@zuzuche.com>
2019-07-09 17:37:44 +0800

    Management

--- Management/managers.py
+++ Management/managers.py
@@ -1,6 +1,40 @@
 from django.db import models
 
 
+class TestCaseManager(models.Manager):
+    def get_object(self, test_case_id):  # 根据parameter得到一条数据
+        return self.get(id=test_case_id)
+
+
 class ParametersManager(models.Manager):
-    def get_objects(self, parameter_id):  # 根据parameter得到一条数据
-        return self.get(user_type_id=parameter_id)
+    def get_object(self, parameter_id):  # 根据parameter得到一条数据
+        return self.get(id=parameter_id)
+
+    @staticmethod
+    def get_parameter_type(self, parameter_type):
+        if parameter_type == 1:
+            return 'key-value'
+        if parameter_type == 2:
+            return 'sql'
+        if parameter_type == 3:
+            return 'testcase_result'
+
+
+class ParameterSqlManager(models.Manager):
+    def get_object(self, parameter_id):  # 根据parameter得到一条数据
+        return self.get(parameter=parameter_id)
+
+
+class ActionManager(models.Manager):
+    def get_object(self, parameter_id):  # 根据parameter得到一条数据
+        return self.get(parameter=parameter_id)
+
+
+class TestCaseParameterManager(models.Manager):
+    def get_object(self, testcase_id):  # 根据parameter得到一条数据
+        return self.get(testcase=testcase_id)
+
+
+class TestSuiteManager(models.Manager):
+    def get_object(self, testsuite_id):  # 根据parameter得到一条数据
+        return self.get(id=testsuite_id)
--- Management/settings.py
+++ Management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Pikachu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test2',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,12 +109,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_L10N = True
-
+USE_L10N = False
+DATE_FORMAT = 'Y-m-d'
+DATETIME_FORMAT = 'Y-m-d H:i:s'
 USE_TZ = True
 
 
